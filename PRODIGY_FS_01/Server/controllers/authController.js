b580c38887d7078d4d98e0f0790b742e4c030736
Zilean12 <aryan.sharma21@st.niituniversity.in>
2024-06-12 18:14:47 +0530

    API Created

---
+++ PRODIGY_FS_01/Server/controllers/authController.js
@@ -0,0 +1,36 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+};
+
+exports.register = async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        const user = await User.create({ username, email, password });
+        const token = generateToken(user._id);
+        res.status(201).json({ token });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (user && (await user.matchPassword(password))) {
+            const token = generateToken(user._id);
+            res.json({ token });
+        } else {
+            res.status(401).json({ message: 'Invalid email or password' });
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.protected = (req, res) => {
+    res.status(200).json({ message: 'Protected route accessed' });
+};
---
+++ PRODIGY_FS_01/Server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// Connect to MongoDB and start server
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running on port ${process.env.PORT}`);
+        });
+    })
+    .catch(err => console.log(err));
